c58876a6f7c6712c95f4513ba59ea99c0d55b0bf
= <=>
2018-06-01 13:44:35 -0300

    correct table name

--- produtos-api/app/infra/ListaDB.js
+++ produtos-api/app/infra/ListaDB.js
@@ -1,9 +1,9 @@
 module.exports = {
     lista: function(conn, callback) {
-        conn.query('select * from lista', callback)
+        conn.query('select * from produtos', callback)
     }
     //,
     // insere: function(conn, item, callback) {
-    //     conn.query(`insert into lista (item) values ('${item}')`)
+    //     conn.query(`insert into produtos (item) values ('${item}')`)
     // }
 }
