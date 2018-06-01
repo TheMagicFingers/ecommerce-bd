aa1c26002eabb4a1478743f4e967a8686174fdf3
= <=>
2018-06-01 13:20:54 -0300

    express config done

--- produtos-api/app/config/express.js
+++ produtos-api/app/config/express.js
@@ -1,1 +1,30 @@
 const express = require('express')
+const load = require('express-load')
+const router = express.Router()
+const bodyParser = require('body-parser')
+
+
+module.exports = () => {
+
+    let app = express()
+
+    app.use(bodyParser.urlencoded({urlencoded: true}))
+    app.use(bodyParser.json())
+
+    // All routes prefixed with /api
+    app.use('/api', router)
+
+    router.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+        next()
+    })
+
+    load('routes', {cwd: 'app'})
+        .then('infra')
+        .into(app)
+
+    return app
+}
