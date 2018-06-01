d40d01a365b16dd461c3e82b9a86a765c517613d
= <=>
2018-06-01 13:33:19 -0300

    express config done

--- produtos-api/app/config/express.js
+++ produtos-api/app/config/express.js
@@ -3,16 +3,13 @@ const load = require('express-load')
 const router = express.Router()
 const bodyParser = require('body-parser')
 
-
 module.exports = () => {
 
     let app = express()
 
     app.use(bodyParser.urlencoded({urlencoded: true}))
     app.use(bodyParser.json())
-
-    // All routes prefixed with /api
-    app.use('/api', router)
+    app.set('json spaces', 4)
 
     router.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
