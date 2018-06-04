98ddc6969bb76343b4c62ef800728fb15e045554
Lu-c-s <Lucas3645@gmail.com>
2018-06-04 18:58:13 -0300

    frontend-app added + produtos route front end

--- produtos-api/db/insert.sql
+++ produtos-api/db/insert.sql
@@ -1,15 +1,15 @@
-insert into produtos (descricao, preco_unit, qtd_est, url_img) values
-  ('Caneca de Porcelana É preciso amar as pessoas como se elas fossem lhe trazer café',
-    34.90, 10, 'http://static.cloud-boxloja.com/lojas/tr8se/produtos/e84074f3-1885-493b-a99a-843092a4693c.jpg'),
+﻿insert into produtos (descricao, preco_unit, qtd_est, url_img) values
+  ('Girl',
+    34.90, 10, 'https://images.unsplash.com/photo-1485199692108-c3b5069de6a0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8bd3737cd07c2ecfdf1cd65c780cfbb3&auto=format&fit=crop&w=750&q=80'),
 
-  ('Caneca de Porcelana No dia mais claro, na noite mais densa, o bug sucumbirá, ante a minha presença',
-    34.90, 12, 'http://static.cloud-boxloja.com/lojas/tr8se/produtos/51486764-8cbc-4de6-aa54-a5e8c840f46d_t260x260.jpg'),
+  ('Dog',
+    35.90, 12, 'https://images.unsplash.com/photo-1447684808650-354ae64db5b8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d3786eeaf183b022befb24523a4e64a0&auto=format&fit=crop&w=747&q=80'),
 
-  ('Caneca de Porcelana É preciso amar as pessoas como se elas fossem lhe trazer café',
-    39.90, 16, '//static.cloud-boxloja.com/lojas/tr8se/produtos/784a3e2d-e564-4c4a-b521-077d53ea9b6d_t260x260.jpg'),
+  ('Iphone',
+    39.90, 16, 'https://images.unsplash.com/photo-1458862768540-8b091824fe2d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=74d87ca88ec668e0d780730b02bf1472&auto=format&fit=crop&w=752&q=80'),
 
-  ('Caneca Térmica Coffe In Code Out',
-    34.90, 10, 'http://static.cloud-boxloja.com/lojas/tr8se/produtos/e84074f3-1885-493b-a99a-843092a4693c.jpg'),
+  (' Code Out',
+    34.90, 1000, 'https://images.unsplash.com/photo-1476170434383-88b137e598bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c68d796381413375485c41ff9294a416&auto=format&fit=crop&w=750&q=80'),
 
-  ('Caneca Térmica PHP',
-    39.90, 30, '//static.cloud-boxloja.com/lojas/tr8se/produtos/a2fa0b2c-8954-463b-b5b4-3f1d58798ac5_t260x260.jpg');
+  ('Drone',
+    19.90, 30, 'https://images.unsplash.com/photo-1515982068348-4e5ce8f0d601?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=351075207b39d4490ed4e8364f6a9ffd&auto=format&fit=crop&w=750&q=80');
